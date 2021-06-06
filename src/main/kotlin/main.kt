efe97bd46b09a51bc695eb91c9c438ab7775999e
Alejandro Cruz <alejandro.cruz@outlook.com>
2021-06-06 11:20:27 -0700

    Comment Code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,8 +11,20 @@ fun main() {
     }
 }
 
+/**
+ * Read file
+ *
+ * @param fileName Read file
+ * @return List of strings read
+ */
 fun readFileAsLinesUsingBufferedReader(fileName: String): List<String> = File(fileName).bufferedReader().readLines()
 
+/**
+ * Main method to sort arrays using MergeSort
+ *
+ * @param A any array
+ * @return Sorted array
+ */
 fun mergeSort(A: IntArray): IntArray {
     val n = A.size
     if (n == 1)
@@ -25,16 +37,26 @@ fun mergeSort(A: IntArray): IntArray {
     return merge(C, D)
 }
 
+/**
+ * Merge two sorted arrays with length (n/2)
+ *
+ * @param C Sorted array
+ * @param D Sorted array
+ * @return Sorted merged array
+ */
 fun merge(C: IntArray, D: IntArray): IntArray {
     var i = 0
     var j = 0
     val B = IntArray(C.size + D.size)
     for (k in B.indices) {
+        // Evaluate when D[j] > D.Size
         if (j == D.size) {
             B[k] = C[i]
             i += 1
             continue
-        } else if (i == C.size) {
+        }
+        // Evaluate when C[i] > C.Size
+        else if (i == C.size) {
             B[k] = D[j]
             j += 1
             continue
